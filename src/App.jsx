f2dea1604fd093c5d74f21752f257a125e76a6fb
marwan <marwnnace4@gmail.com>
2025-05-16 22:27:34 +0700

    Commit Terbaru

--- src/App.jsx
+++ src/App.jsx
@@ -25,21 +25,21 @@ function App() {
     <Container>
       <NavbarComponent />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/berita" element={<Berita />} />
-        <Route path="/berita/:id" element={<BeritaDetail />} />
-        <Route path="/berita/halaman/:halaman" element={<Berita />} />
-        <Route path="/event" element={<Event />} />
-        <Route path="/panduan" element={<Panduan />} />
-        <Route path="/kegiatan" element={<Kegiatan />} />
-        <Route path='/kegiatan/:halaman' element={<Kegiatan />} />
-        <Route path='/karir' element={<Karir />} />
-        <Route path='/karirDetail' element={<KarirDetail />} />
-        <Route path='/rfc' element={<RFC />} />
-        <Route path='/laporan' element={<Laporan />} />
-        <Route path='/kontak' element={<Kontak />} />
-        <Route path='/statistik' element={<Statistik />} />
+        <Route path="/projectMagang" element={<Home />} />
+        <Route path="/projectMagang/profile" element={<Profile />} />
+        <Route path="/projectMagang/berita" element={<Berita />} />
+        <Route path="/projectMagang/berita/:id" element={<BeritaDetail />} />
+        <Route path="/projectMagang/berita/halaman/:halaman" element={<Berita />} />
+        <Route path="/projectMagang/event" element={<Event />} />
+        <Route path="/projectMagang/panduan" element={<Panduan />} />
+        <Route path="/projectMagang/kegiatan" element={<Kegiatan />} />
+        <Route path='/projectMagang/kegiatan/:halaman' element={<Kegiatan />} />
+        <Route path='/projectMagang/karir' element={<Karir />} />
+        <Route path='/projectMagang/karirDetail' element={<KarirDetail />} />
+        <Route path='/projectMagang/rfc' element={<RFC />} />
+        <Route path='/projectMagang/laporan' element={<Laporan />} />
+        <Route path='/projectMagang/kontak' element={<Kontak />} />
+        <Route path='/projectMagang/statistik' element={<Statistik />} />
       </Routes>
       <Footer />
     </Container>
