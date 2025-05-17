df9d960862dd3b5a8e349cb02e094d04254aa191
marwan <marwnnace4@gmail.com>
2025-05-17 10:22:10 +0700

    Commit Terbaru

--- src/main.jsx
+++ src/main.jsx
@@ -20,7 +20,7 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter >
       <ScrollToTop />
       <App />
     </BrowserRouter>
