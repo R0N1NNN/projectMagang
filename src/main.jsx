c2b02b2e20117aae1c1f71d488d6fc717aa55698
marwan <marwnnace4@gmail.com>
2025-05-18 07:25:08 +0700

    Commit Terbaru

--- src/main.jsx
+++ src/main.jsx
@@ -16,13 +16,13 @@ AOS.init({
   mirror: true,
 });
 
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter >
+    <HashRouter >
       <ScrollToTop />
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
