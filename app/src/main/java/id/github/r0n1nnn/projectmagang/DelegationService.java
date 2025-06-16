ce692e68b4361408bb40cb4ff7c9ca7e7d04dd94
marwan <marwnnace4@gmail.com>
2025-06-16 10:46:45 +0700

    Commit Terbaru

---
+++ app/src/main/java/id/github/r0n1nnn/projectmagang/DelegationService.java
@@ -0,0 +1,14 @@
+package id.github.r0n1nnn.projectmagang;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
