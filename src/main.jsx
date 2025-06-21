1359e4e7cc7f3eb34e5bccd6cfe5363638efcce7
marwan <marwnnace4@gmail.com>
2025-06-21 16:36:40 +0700

    Login Dengan Google

--- src/main.jsx
+++ src/main.jsx
@@ -23,11 +23,11 @@ import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId="617597158120-7aot5v7058n3tc8a4o4e5csq9m61o3j7.apps.googleusercontent.com">...
+    <GoogleOAuthProvider clientId="617597158120-7aot5v7058n3tc8a4o4e5csq9m61o3j7.apps.googleusercontent.com">
       <HashRouter >
         <ScrollToTop />
         <App />
       </HashRouter>
-    </GoogleOAuthProvider>;
+    </GoogleOAuthProvider>
   </React.StrictMode>
 )
--- src/pages/AuthLayout.jsx
+++ src/pages/AuthLayout.jsx
@@ -1,11 +1,7 @@
 import { Outlet } from 'react-router-dom';
 
 function AuthLayout() {
-    return (
-        <div className="auth-wrapper">
-            <Outlet />
-        </div>
-    );
+    return <Outlet />
 }
 
 export default AuthLayout;
